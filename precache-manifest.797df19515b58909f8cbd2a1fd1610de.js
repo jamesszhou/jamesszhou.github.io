b4e93b5370266d4d4efff142fb7bac9eba8905b1
James Zhou <jameszhou@g.ucla.edu>
2021-09-15 23:12:51 -0700

    Updates

---
+++ precache-manifest.797df19515b58909f8cbd2a1fd1610de.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531e673a3d98e62970d71e58c36f7648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc4923d7bd686e03c8fb",
+    "url": "/static/css/main.a76c431b.chunk.css"
+  },
+  {
+    "revision": "8fcb953cd0d331008acc",
+    "url": "/static/js/2.27b7870d.chunk.js"
+  },
+  {
+    "revision": "fc4923d7bd686e03c8fb",
+    "url": "/static/js/main.09e596a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "29d3e2ac9ecbfd9f6d349d9013414729",
+    "url": "/static/media/Photography1.29d3e2ac.jpg"
+  },
+  {
+    "revision": "11831f65a35e46cd3a2c511e62304f09",
+    "url": "/static/media/Photography2.11831f65.jpg"
+  },
+  {
+    "revision": "7777aa6e864d77ed3158c980c78e8d5c",
+    "url": "/static/media/Photography3.7777aa6e.jpg"
+  },
+  {
+    "revision": "6ec47d785361d2623d1d244b8493c2b4",
+    "url": "/static/media/Photography4.6ec47d78.JPG"
+  },
+  {
+    "revision": "a7a0e7e515951622aac7bed42b19db30",
+    "url": "/static/media/Photography5.a7a0e7e5.jpg"
+  },
+  {
+    "revision": "dcf4eb25e8b7cc503296f7b6aa752096",
+    "url": "/static/media/aws.dcf4eb25.png"
+  },
+  {
+    "revision": "70cc9991c72e15f813fe9282d3ea46d9",
+    "url": "/static/media/codeinspector.70cc9991.png"
+  },
+  {
+    "revision": "678fb88e7d8f08877d443fd475057bed",
+    "url": "/static/media/db-logo.678fb88e.png"
+  },
+  {
+    "revision": "e4e44af322f98f5f0990ae80599f12d7",
+    "url": "/static/media/file.e4e44af3.png"
+  },
+  {
+    "revision": "534ae36825d298c41d9f8ddf6675d6c4",
+    "url": "/static/media/file:repo.534ae368.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "57d93bcea794d1cb53af47ea38d31c58",
+    "url": "/static/media/portrait.57d93bce.jpg"
+  },
+  {
+    "revision": "79eb89103aa37ed1fa45d9ce442fd527",
+    "url": "/static/media/resume.79eb8910.pdf"
+  },
+  {
+    "revision": "44bb732b8a345a1822d405f1df1f16ae",
+    "url": "/static/media/sfBackground.44bb732b.JPG"
+  },
+  {
+    "revision": "123584f785f8ac95629b185770323a1d",
+    "url": "/static/media/uber.123584f7.svg"
+  },
+  {
+    "revision": "77c37bb305166892f0c24e72faba02ad",
+    "url": "/static/media/ultimate1.77c37bb3.gif"
+  },
+  {
+    "revision": "89363c18a9258509a7ca677a167410ac",
+    "url": "/static/media/ultimate2.89363c18.jpg"
+  },
+  {
+    "revision": "1e1cf4655083b070c28a2ecf9020189f",
+    "url": "/static/media/ultimate3.1e1cf465.jpg"
+  },
+  {
+    "revision": "6272695aefc1d43a2525b038d659fdfe",
+    "url": "/static/media/zombieDash.6272695a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f70e469dc6ab0b4dd76ff951f708c43a.js"
+  "/precache-manifest.797df19515b58909f8cbd2a1fd1610de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
